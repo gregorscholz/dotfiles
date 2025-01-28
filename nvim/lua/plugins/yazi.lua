e6f0b27dfc1e7c624aeb174d3285118caa3586bf
gscholz <gregorscholz@proton.me>
2025-01-28 12:45:00 +0100

    add nvim yazi config

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,24 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>-",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			"<leader>cw",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+	},
+	opts = {
+		-- if you want to open yazi instead of netrw, see below for more info
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+}
