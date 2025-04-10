d3780bce0665824f1f5ab0653c621ee0dd6c372d
gscholz <gregor_walter.scholz@stud.htwk-leipzig.de>
2025-04-10 20:02:30 +0200

    [add] xml formatter, linter and lsp

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,21 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			kotlin = { "ktfmt" },
-		},
-		default_format_opts = {
-			lsp_format = "fallback",
-		},
-		format_after_save = {
-			lsp_format = "fallback",
-		},
-		formatters = {
-			ktfmt = {
-				prepend_args = { "--kotlinlang-style" },
-			},
-		},
-	},
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            kotlin = { "ktfmt" },
+            xml = { "xmllint" },
+            ["*.sdf"] = { "xmllint" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        format_after_save = {
+            lsp_format = "fallback",
+        },
+        formatters = {
+            ktfmt = {
+                prepend_args = { "--kotlinlang-style" },
+            },
+        },
+    },
 }
